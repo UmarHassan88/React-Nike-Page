cb71ab8095f26cca764613754ec97ff88164790a
UmarHassan88 <syedumarhasanzia@gmail.com>
2024-05-01 00:42:15 +0500

    Initial Commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Navigation from './Components/Navigation'
+import Main from './Components/Main'
+
+export default function App() {
+  return (
+    <div>   
+     <Navigation />
+     <Main />
+    </div>
+  )
+}
---
+++ vite-project/src/Components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='container'>
+        <section className='sec1'>
+<h1 className='head'>Your Feet Deserves the Best</h1>
+<h6>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO 
+    HELP YOU WITH OUR SHOES.YOUR FEET DESERVE 
+    THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</h6>
+    <div className='containernested'>
+    <button className='btn-styling btn1' >Shop Now</button>
+    <button className='btn2'>Category</button>
+    </div>
+    <div>
+        <h6>Also Available On</h6>
+       
+        <img src="/flipkart.png" alt="flip" className='image1' />
+        <img src="/amazon.png" alt="flip" className='image2' />
+    </div>
+        </section>
+        <section className='sec2'>
+        <img src="/shoe_image.png" alt="shoe" />
+        </section>
+     
+    </div>
+  )
+}
